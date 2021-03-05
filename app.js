b4d0ba1ce3b2eac98a4f1a7cb64b008a4e560446
Toqui012 <alvaroseba2000@gmail.com>
2021-03-05 00:03:28 -0300

    nuevo navbar

--- app.js
+++ app.js
@@ -14,18 +14,44 @@ function pintar(){
         {
             console.log("Movimiento Afuera")
             _navItem.style.transition = "0.3s"
-            _navItem.style.color = "rgba(0,0,0,.55)";
+            _navItem.style.color = "red";
         })
 
     })
 
 }
 
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+    
+}
+
+
+
 function caca(){
     console.log("hola")
 }
 
 
+navSlide();
 pintar()
 
 
